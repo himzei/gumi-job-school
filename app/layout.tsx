198c794c562572c835c5479e3a35285c5d586c28
himzei <himzei@gmail.com>
2025-02-05 09:27:26 +0900

    meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,30 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "한국직업교육학원",
-  description: "구미 한국직업 교육학원",
+  description:
+    "경북 구미 훈련생을 위한 한국직업교육학원! 교육과 취업을 동시에 잡으세요!!",
+  openGraph: {
+    title: "한국직업교육학원",
+    description:
+      "경북 구미 훈련생을 위한 최고의 교육 기회! 교육과 취업을 동시에 잡으세요!",
+    url: "https://gumi-job-school.vercel.app", // 실제 URL로 변경
+    siteName: "한국직업교육학원",
+    images: [
+      {
+        url: "@/public/logo_s.png", // 이미지 경로
+        width: 1200,
+        height: 630,
+        alt: "한국직업교육학원",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "한국직업교육학원",
+    description: "교육과 취업을 동시에 잡으세요!",
+    images: ["/images/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({
