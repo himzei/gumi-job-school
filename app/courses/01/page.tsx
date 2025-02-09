5dcdab9b749fb4343d1e1ea0e0c0044bd673ccdd
himzei <himzei@gmail.com>
2025-02-10 01:19:57 +0900

    re

--- app/courses/01/page.tsx
+++ app/courses/01/page.tsx
@@ -1,18 +1,10 @@
-import TitleSection from "@/app/introduce/components/TitleSection";
 import Poster01 from "@/public/courses/001.jpg";
 import Image from "next/image";
 
 export function Courses01() {
   return (
     <>
-      <div className="max-w-3xl w-full mx-auto my-8">
-        <TitleSection
-          // section="contact"
-          mainTitle="구미시 구미중앙로27길 15"
-          subTitle="한국직업교육학원"
-          // subTitle2="문의주세요"
-        />
-      </div>
+      <div className="max-w-3xl w-full mx-auto my-8"></div>
       <div className="w-full h-full">
         <div className="max-w-5xl w-full mx-auto">
           <Image
