db6667c85fc19b16bfc86d62cbd1610ab9d7178f
himzei <himzei@gmail.com>
2025-02-10 01:29:09 +0900

    1234

--- app/courses/01/page.tsx
+++ app/courses/01/page.tsx
@@ -1,10 +1,17 @@
 import Poster01 from "@/public/courses/001.jpg";
 import Image from "next/image";
 
-export function Courses01() {
+export default function Courses01() {
   return (
     <>
-      <div className="max-w-3xl w-full mx-auto my-8"></div>
+      <div className="max-w-3xl w-full mx-auto my-8">
+        <h1 className="text-3xl font-light tracking-tight text-center">
+          빅데이터기반의 멀티사무관리원 양성과정
+        </h1>
+        <p className="text-muted-foreground text-center">
+          구미 회계 컴퓨터 학원 ADsP + 전산세무회계(FAT1급, TAT2급)
+        </p>
+      </div>
       <div className="w-full h-full">
         <div className="max-w-5xl w-full mx-auto">
           <Image
