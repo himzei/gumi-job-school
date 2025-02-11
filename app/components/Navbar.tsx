9b170259c65bf157bcbe51e43cf5ee7eb4ea1a9c
himzei <himzei@gmail.com>
2025-02-11 21:35:35 +0900

    상단메뉴

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,6 +10,7 @@ import {
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import prisma from "../utils/db";
 import { Hero } from "./frontend/Hero";
+import { Award } from "lucide-react";
 
 export default async function Navbar() {
   const { getUser } = getKindeServerSession();
@@ -34,15 +35,21 @@ export default async function Navbar() {
       <div className="max-w-7xl mx-auto w-full h-24 flex justify-center z-10 ">
         <div className="relative w-full h-full flex justify-between items-center text-xs">
           {/* 1. 로그인 회원가입 */}
-          <div className="hidden md:flex uppercase h-full px-4 items-center space-x-2">
-            <Image
-              src={Logo}
-              alt="logo small"
-              width={20}
-              height={20}
-              className="translate-x-1 grayscale"
-            />
+          <div className="flex space-x-2">
+            <div className="flex items-center w-8 bg-primary rounded-lg p-0">
+              <Award className="size-8 text-white" />
+            </div>
+
+            <div className="flex flex-col space-y-0">
+              <h1 className="flex text-md items-center font-light tracking-tight translate-y-0.5">
+                구미 회계 컴퓨터 전문학원
+              </h1>
+              <p className="flex text-lg font-bold tracking-tight translate-y-0.5">
+                2025년 취업교육! 최고의 선택!!
+              </p>
+            </div>
           </div>
+
           {/* 2. 로고 */}
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <Link href="/" className="flex items-center space-x-2">
